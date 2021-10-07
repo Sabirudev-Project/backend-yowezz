ac25b253b6e5856517f4594f2e51b5f696567b77
baguzzza <bagusracmad@gmail.com>
2021-10-08 05:26:24 +0700

    Update Message Controller

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1/users')->group(function () {
+    //users
+    Route::get('messages', [Users::class, 'fetch_message']);
+    Route::post('messages', [Users::class, 'send_message']);
 });
