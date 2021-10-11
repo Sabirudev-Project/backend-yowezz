574d5369aa15f5aaea459cb0c603a50554715942
baguzzza <bagusracmad@gmail.com>
2021-10-11 23:36:19 +0700

    Update CRUD with Upload

--- app/Http/Controllers/DISC/UploadFile.php
+++ app/Http/Controllers/DISC/UploadFile.php
@@ -9,17 +9,17 @@ use Illuminate\Support\Facades\File;
 class UploadFile extends Controller
 {
     //
-    public static function upload_file(Request $request, $string_var, $id)
+    public static function upload_file(Request $request, $table, $string_var, $id)
     {
         if ($request->hasFile($string_var)) {
             $file = $request->file($string_var);
-            $destination_upload = public_path($string_var . '/');
+            $destination_upload = public_path($table . '/' . $string_var . '/');
             if (!file_exists($destination_upload)) {
                 File::makeDirectory($destination_upload, 0777, true);
                 // mkdir($destination_upload, 0777, true);
             }
             $file->move($destination_upload, $string_var . $id . '.' . $file->getClientOriginalExtension());
-            $imageUrl = 'public/images' . '/' . $string_var  . $id . '.' . $file->getClientOriginalExtension();
+            $imageUrl = 'public' . '/' . $table . '/' . $string_var . '/' . $string_var . $id . '.' . $file->getClientOriginalExtension();
             return $imageUrl;
         } else {
             return false;
